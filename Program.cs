24f9e23b7939b2e679f6aaf8aae8ed06a79fc7b6
steven-buettner44 <66680160+steven-buettner44@users.noreply.github.com>
2020-06-10 17:45:04 -0500

    Add files via upload
    conotso university

---
+++ Program.cs
@@ -0,0 +1,63 @@
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+//using ContosoUniversity.Pages.Shared;
+using ContosoUniversity.Data;
+using Microsoft.Extensions.DependencyInjection;
+*/
+using ContosoUniversity.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+
+namespace ContosoUniversity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // CreateHostBuilder(args).Build().Run();
+             var host = CreateHostBuilder(args).Build();
+             CreateDbIfNotExists(host);
+            host.Run();
+        }
+        private static void CreateDbIfNotExists(IHost host)
+        {
+           using ( var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+
+                    //  var context = services.GetRequiredService<SchoolContext>();
+                    // context.Database.EnsureCreated();
+                    var context = services.GetRequiredService<SchoolContext>();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService <ILogger< Program >> ();
+                    logger.LogError(ex, "an Error Occured creating the database");
+                }
+            }
+        }
+                
+
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
